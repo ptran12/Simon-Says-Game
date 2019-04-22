ce7e810d93bc68bdcc1b48050bdc7778bc16248d
Phi Tran <phitran@Phis-MacBook-Pro.local>
2019-04-22 18:02:46 -0500

    logic of game started, wip

---
+++ game.js
@@ -0,0 +1,14 @@
+ var gamePattern = [];
+ var buttonColors = ["Red", "Green", "Blue", "Yellow"];
+
+ function newSequence() {
+   var randomNumber = Math.floor(Math.random() * 4);
+   var randomChosenColor = buttonColors[randomNumber];
+   gamePattern.push(randomChosenColor)
+   console.log(randomChosenColor);
+}
+ 
+
+newSequence();
+console.log(gamePattern);
+
