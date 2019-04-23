f035d27cad90c5254b95921f0369ff4b3af40082
Phi Tran <phitran.722@gmail.com>
2019-04-23 17:50:39 -0500

    when clicking on a color, it is returning the id

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
  var gamePattern = [];
  var buttonColors = ["Red", "Green", "Blue", "Yellow"];
+ var userClickedPattern = [];
 
  function newSequence() {
    var randomNumber = Math.floor(Math.random() * 4);
@@ -7,12 +8,17 @@
    gamePattern.push(randomChosenColor)
   //  console.log(randomChosenColor);
 
-  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  // $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
-  audio.play();
+  // var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  // audio.play();
     
 }
  
+$(".btn").click(function(event){
+  var userChosenColor = event.target.id
+  console.log(userChosenColor);
+});
+
 newSequence();
 console.log(gamePattern);
