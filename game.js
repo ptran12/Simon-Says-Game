fff2b83aaec1c27c48f0b06e7462f520ae703fd6
Phi Tran <phitran.722@gmail.com>
2019-04-24 00:10:55 -0500

    animation working for sequence

--- game.js
+++ game.js
@@ -1,37 +1,58 @@
- var gamePattern = [];
- var buttonColors = ["Red", "Green", "Blue", "Yellow"];
- var userClickedPattern = [];
 
- var started = false;
- var level = 0;
+var buttonColors = ["red", "blue", "green", "yellow"];
 
- $(document).keypress(function() {
-  if(!started) {
-    
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
     $("#level-title").text("Level " + level);
-    newSequence();
+    nextSequence();
     started = true;
   }
 });
 
+$(".btn").click(function() {
 
-$(".btn").click(function(event){
-  var userChosenColor = event.target.id
-  // console.log(userChosenColor);
-  userClickedPattern.push(userChosenColor);
-  console.log(userClickedPattern)
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
 
-  playSound(userChosenColor);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
 
-  animatePress(userChosenColor);
-
+  checkAnswer(userClickedPattern.length-1);
 });
 
+function checkAnswer(currentLevel) {
 
-function newSequence() {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      if (userClickedPattern.length === gamePattern.length){
+
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+
+      console.log("wrong");
+
+    }
+
+}
+
+function nextSequence() {
+
+  userClickedPattern = [];
 
   level++;
-
   $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
@@ -40,24 +61,18 @@ function newSequence() {
 
   $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColor);
-  
 }
 
-// function to play the sound 
-
-function playSound (name) {
-
-  $("#" + name).fadeIn(100).fadeOut(100).fadeIn(100);
-
+function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-// Press Animation
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
-
-  setTimeout(function(){
+  setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
-  },100);
+  }, 100);
 }
+
+
